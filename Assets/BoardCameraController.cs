f8093bbfa6408b8aa924a90890f63499f5ede8fd
Michel Zimmer <mzimmer@uni-bremen.de>
2016-01-14 17:39:33 +0100

    Basic FlightBoard controlls

---
+++ Assets/BoardCameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardCameraController : MonoBehaviour {
+    public GameObject board;
+
+    void Start() {
+        transform.eulerAngles = Vector3.up;
+    }
+
+	void Update () {
+        transform.position = new Vector3(board.transform.position.x, board.transform.position.y + 1, board.transform.position.z - 4);
+	}
+}
--- Assets/FlightBoardController.cs
+++ Assets/FlightBoardController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FlightBoardController : MonoBehaviour {
     public float speed;
+    public float rotationSpeed;
+    public float maxRotLeftRight;
+    public float maxRotUpDown;
     public float lvlGenerationBorderLeft;
     public float lvlGenerationBorderSky;
     public float lvlGenerationBorderRight;
@@ -11,14 +14,15 @@ public class FlightBoardController : MonoBehaviour {
     public int lvlGenerationBackwards;
     public int lvlGenerationInAdvance;
     public float lvlGenerationMeshDistance;
+    public Material lvlGenerationBaseMaterial;
     private Mesh lvlGenerationTestMesh;
     private List<GameObject> lvlGenerationGameObjects;
 
 	void Start () {
         lvlGenerationGameObjects = new List<GameObject>();
         lvlGenerationCurrent = -lvlGenerationBackwards;
-        transform.position = new Vector3(0.0f, 0.0f, 0.0f);
-        transform.up = Vector3.up;
+        transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+        transform.eulerAngles = Vector3.up;
         Mesh mesh = new Mesh();
         {
             mesh.vertices = new Vector3[]{
@@ -38,14 +42,44 @@ public class FlightBoardController : MonoBehaviour {
         lvlGenerationTestMesh = mesh;
     }
 
-	void Update () {
-        transform.Translate(Vector3.forward * speed);
+	void Update ()
+    {
+        Vector3 r = transform.eulerAngles;
+        if (Input.GetKey(KeyCode.W)) {
+            r.x -= rotationSpeed;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            r.x += rotationSpeed;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            r.z += rotationSpeed;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            r.z -= rotationSpeed;
+        }
+        if (r.z > maxRotLeftRight && r.z < 180) {
+            r.z = maxRotLeftRight;
+        }
+        if (r.z < 360 - maxRotLeftRight && r.z > 180) {
+            r.z = 360 - maxRotLeftRight;
+        }
+        if (r.x > maxRotUpDown && r.x < 180) {
+            r.x = maxRotUpDown;
+        }
+        if (r.x < 360 - maxRotUpDown && r.x > 180) {
+            r.x = 360 - maxRotUpDown;
+        }
+        transform.eulerAngles = r;
+        Vector3 p = transform.position;
+        p.z += speed;
+        transform.position = p;
         int lvlGenerationUpTo = Convert.ToInt32(Math.Ceiling(transform.position.z / lvlGenerationMeshDistance)) + lvlGenerationInAdvance;
         while (lvlGenerationCurrent < lvlGenerationUpTo) {
             GameObject lvlGenerationGameObject = new GameObject();
             lvlGenerationGameObject.AddComponent<MeshFilter>().sharedMesh = lvlGenerationTestMesh;
             lvlGenerationGameObject.AddComponent<MeshRenderer>();
             lvlGenerationGameObject.transform.position = Vector3.forward * lvlGenerationCurrent * lvlGenerationMeshDistance;
+            lvlGenerationGameObject.GetComponent<Renderer>().material = lvlGenerationBaseMaterial;
             lvlGenerationGameObjects.Add(lvlGenerationGameObject);
             ++lvlGenerationCurrent;
         }
