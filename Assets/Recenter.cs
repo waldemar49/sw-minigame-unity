8df09dcb36859f116847a93048152c53a4fde877
Michel Zimmer <mzimmer@uni-bremen.de>
2016-01-21 14:57:07 +0100

    Add: Oculus support

---
+++ Assets/Recenter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Recenter : MonoBehaviour {
+
+    public KeyCode[] recenter;
+
+    void Update() {
+        if (allDown(recenter)) {
+            UnityEngine.VR.InputTracking.Recenter();
+        }
+    }
+
+    private bool allDown(KeyCode[] keys) {
+        foreach (KeyCode key in keys) {
+            if (!Input.GetKey(key)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
